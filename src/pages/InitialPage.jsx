6095aa9c44d603cc4d546f5656abefabbf7b465c
FabricioSobrinho <fabriciomails3@gmail.com>
2024-09-21 21:06:19 -0400

    wip: create initial page

---
+++ src/pages/InitialPage.jsx
@@ -0,0 +1,27 @@
+import styles from "../styles/InitialPageStyles.module.css";
+import Button from "../components/forms/Button";
+import Input from "../components/forms/Input";
+
+function InitialPage() {
+  return (
+    <div className={styles.initialPage}>
+      <div className={styles.leftInitialPage}>
+        <h1>Bem vindo de Voltaa!</h1>
+        <p>Faça login para se conectar</p>
+        <Button text={"LOGIN"} handleClick={() => alert("Sanity test")} />
+      </div>
+      <div className={styles.rigthInitialPage}>
+        <h1>Novo por aqui?</h1>
+        <h3>Crie sua conta!</h3>
+        <div className={styles.form}>
+          <Input placeholder={"Nome da barraca"} />
+          <Input placeholder={"Senha"} type={"password"} />
+          <Input placeholder={"Confirme sua senha"} type={"password"} />
+          <Button text={"CRIAR CONTA"} color="green"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InitialPage;
