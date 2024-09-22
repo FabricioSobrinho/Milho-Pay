397056a976e94454ce696732018e31932b0248ae
FabricioSobrinho <fabriciomails3@gmail.com>
2024-09-21 21:04:51 -0400

    Feat: created initial page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import InitialPage from "./pages/initialPage";
+
+function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<InitialPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
