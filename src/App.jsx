864bba3b99c8bd6bc3e660c6b3887d8dd8e09c9a
FabricioSobrinho <fabriciomails3@gmail.com>
2024-09-28 17:18:45 -0400

    Feat: add order page route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import InitialPage from "./pages/initialPage";
 import LoginPage from "./pages/LoginPage";
+import OrderPage from "./pages/OrderPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<InitialPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/order" element={<OrderPage />} />
         </Routes>
       </Router>
     </>
