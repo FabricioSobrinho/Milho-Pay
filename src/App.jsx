992aa6c4ba748e8a9f28736eb26240491c1e08ff
FabricioSobrinho <fabriciomails3@gmail.com>
2024-10-05 14:32:31 -0400

    Feat: add initial panel, drinks and menu cad pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InitialPage from "./pages/initialPage";
 import LoginPage from "./pages/LoginPage";
 import OrderPage from "./pages/OrderPage";
+import MenuCadPage from "./pages/MenuCadPage";
+import DrinkCadPage from "./pages/DrinkCadPage";
+import InitialPanel from "./pages/initialPanel";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
           <Route exact path="/" element={<InitialPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/order" element={<OrderPage />} />
+          <Route path="/menu" element={<MenuCadPage />} />
+          <Route path="/drink" element={<DrinkCadPage />} />
+          <Route path="/tent" element={<InitialPanel />} />
         </Routes>
       </Router>
     </>
