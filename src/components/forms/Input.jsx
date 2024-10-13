efb42b6231661adeb471cc143fe5ff5bf748b711
FabricioSobrinho <fabriciomails3@gmail.com>
2024-10-13 19:46:18 -0400

    Feat: add name to input

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,10 +1,15 @@
 import styles from "../../styles/InputStyles.module.css";
 
 // eslint-disable-next-line react/prop-types
-function Input({ placeholder, handleChange, type}) {
+function Input({ placeholder, handleChange, type, name }) {
   return (
     <div className={styles.input}>
-      <input placeholder={placeholder} onChange={handleChange} type={type}/>
+      <input
+        placeholder={placeholder}
+        onChange={handleChange}
+        type={type}
+        name={name}
+      />
     </div>
   );
 }
