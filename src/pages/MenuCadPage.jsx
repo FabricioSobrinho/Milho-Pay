f1e2a358c11c222085c261ba05642c22632b708b
FabricioSobrinho <fabriciomails3@gmail.com>
2024-10-14 19:32:33 -0400

    Feat: add menu insert

--- src/pages/MenuCadPage.jsx
+++ src/pages/MenuCadPage.jsx
@@ -1,17 +1,71 @@
+import { useState } from "react";
+import { useBaseUrl } from "../hooks/useBaseUrl";
+import axios from "axios";
+
 import styles from "../styles/MenuCadPage.module.css";
 
 import Input from "../components/forms/Input";
 import Button from "../components/forms/Button";
 
 function MenuCadPage() {
+  const { baseUrl, authToken } = useBaseUrl();
+  const [menuData, setMenuData] = useState({
+    name: "",
+    value: 0,
+    dishCost: 0,
+  });
+
+  const handleMenuData = (e) => {
+    if (e.target.type == "number") {
+      setMenuData({ ...menuData, [e.target.name]: Number(e.target.value) });
+    } else {
+      setMenuData({ ...menuData, [e.target.name]: e.target.value });
+    }
+
+    console.log(menuData);
+    
+  };
+
+  const insertMenu = async () => {
+    try {
+      const response = await axios.post(
+        `${baseUrl}/dishs`,
+        menuData,
+        authToken
+      );
+
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className={styles.mainMenuCad}>
       <h1>Cadastre seus pratos!</h1>
       <div className={styles.cadForm}>
-        <Input placeholder={"Nome do Prato"} />
-        <Input placeholder={"Custo do Prato"} />
-        <Input placeholder={"Valor de venda"} />
-        <Button text={"Incluir Prato"} type={"green"} />
+        <Input
+          placeholder={"Nome do Prato"}
+          name={"name"}
+          handleChange={handleMenuData}
+        />
+        <Input
+          placeholder={"Custo do Prato"}
+          name={"value"}
+          type={"number"}
+          handleChange={handleMenuData}
+        />
+        <Input
+          placeholder={"Valor de venda"}
+          name={"dishCost"}
+          type={"number"}
+          handleChange={handleMenuData}
+        />
+        <Button
+          text={"Incluir Prato"}
+          type={"green"}
+          handleClick={insertMenu}
+        />
       </div>
     </div>
   );
