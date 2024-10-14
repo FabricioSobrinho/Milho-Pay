3d98f5922a23fb2ccd3996e7df99cff8edd691dd
FabricioSobrinho <fabriciomails3@gmail.com>
2024-10-14 19:31:55 -0400

    Feat: add token into hook

--- src/hooks/useBaseUrl.jsx
+++ src/hooks/useBaseUrl.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react';
-
+import { useState } from "react";
+import Cookies from "js-cookie";
 export const useBaseUrl = () => {
-  const [baseUrl, setBaseUrl] = useState('http://localhost:3000');
+  const [baseUrl, setBaseUrl] = useState("http://localhost:3000");
+
+  const token = Cookies.get("loginToken");
+
   const config = {
     headers: {
-      "Content-type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
   };
-  return { baseUrl, setBaseUrl, config };
-};
 
+  const authToken = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  return { baseUrl, setBaseUrl, config, authToken };
+};
