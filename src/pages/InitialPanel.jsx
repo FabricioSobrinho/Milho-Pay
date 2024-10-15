ab4eb39018b914c7d0338f7c919c2ddf3ebf3210
FabricioSobrinho <fabriciomails3@gmail.com>
2024-10-15 19:47:32 -0400

    Feat: add route navigation

--- src/pages/InitialPanel.jsx
+++ src/pages/InitialPanel.jsx
@@ -1,19 +1,29 @@
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+
 import styles from "../styles/InitialPanel.module.css";
 
 import Button from "../components/forms/Button";
 
 function InitialPanel() {
+  const isLogged = Cookies.get("loginToken");
+
+  const navigate = useNavigate();
   return (
     <div className={styles.mainInitialPanel}>
-      <h1>Bem vindos Barraca!</h1>
-      <h3>O que vamos fazer?</h3>
-      <div className={styles.form}>
-        <Button text={"Cadastrar pratos"} type={"green"}/>
-        <Button text={"Cadastrar bebidas"} type={"green"}/>
-        <Button text={"Tela de vendas"} type={"green"}/>
-      </div>
+      {isLogged && (
+        <>
+          <h1>Bem vindos Barraca!</h1>
+          <h3>O que vamos fazer?</h3>
+          <div className={styles.form}>
+            <Button text={"Cadastrar pratos"} type={"green"} handleClick={() => navigate("/menu")}/>
+            <Button text={"Cadastrar bebidas"} type={"green"} handleClick={() => navigate("/drink")}/>
+            <Button text={"Tela de vendas"} type={"green"} handleClick={() => navigate("/order")}/>
+          </div>
+        </>
+      )}
     </div>
   );
-};
+}
 
 export default InitialPanel;
