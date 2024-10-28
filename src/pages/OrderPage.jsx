bddac849094babef473e3d9a7e83e3e4332bd586
FabricioSobrinho <fabriciomails3@gmail.com>
2024-10-28 13:37:09 -0400

    Feat: implement sell function

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useBaseUrl } from "../hooks/useBaseUrl";
+
 import styles from "../styles/OrderPage.module.css";
 
 import Button from "../components/forms/Button";
@@ -5,37 +8,169 @@ import Input from "../components/forms/Input";
 import Select from "../components/forms/Select";
 
 import OrderItem from "../components/OrderItem";
+import { useEffect, useState } from "react";
 
 function OrderPage() {
+  const { baseUrl, authToken } = useBaseUrl();
+  const [paymentMethodId, setPaymentMethodId] = useState(0);
+
+  const [dishs, setDishs] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+
+  const [totalValue, setTotalValue] = useState(0);
+
+  const [menuDish, setMenuDish] = useState({ dishId: 0, quantity: 0 });
+  const [menuDrink, setMenuDrink] = useState({ drinkId: 0, quantity: 0 });
+
+  const [sendDish, setSendDish] = useState([]);
+  const [sendDrink, setSendDrink] = useState([]);
 
   const options = [
     {
-      nome: "Crédito",
-      id: 1
+      name: "Crédito",
+      id: 1,
     },
     {
-      nome: "Débito",
-      id: 2
+      name: "Débito",
+      id: 2,
     },
     {
-      nome: "Dinheiro",
-      id: 3
+      name: "Dinheiro",
+      id: 3,
     },
-  ]
+    {
+      name: "Pix",
+      id: 4,
+    },
+  ];
+
+  const handleDrinkMenu = (e) => {
+    setMenuDrink({ ...menuDrink, [e.target.name]: Number(e.target.value) });
+  };
+
+  const handleDishMenu = (e) => {
+    setMenuDish({ ...menuDish, [e.target.name]: Number(e.target.value) });
+  };
+
+  const loadDishs = async () => {
+    try {
+      const response = await axios.get(`${baseUrl}/dishs`, authToken);
+
+      setDishs((prev) => [...response.data]);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handlePaymentMethod = (e) => {
+    setPaymentMethodId(e.target.value);
+    console.log(paymentMethodId);
+  };
+
+  const addDrink = () => {
+    setSendDrink((prev) => [...prev, menuDrink]);
+
+    const drink = drinks.filter((item) => item.drinkId == menuDrink.id);
+
+    const value = drink[0].value * menuDrink.quantity;
+
+    setTotalValue((prev) => prev + value);
+  };
+
+  const addDish = () => {
+    setSendDish((prev) => [...prev, menuDish]);
+
+    const dish = dishs.filter((item) => item.dishId == menuDish.id);
+    const value = dish[0].value * menuDish.quantity;
+
+    setTotalValue((prev) => prev + value);
+  };
+
+  const loadDrinks = async () => {
+    try {
+      const response = await axios.get(`${baseUrl}/drinks`, authToken);
+
+      setDrinks((prev) => [...response.data]);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const makeSell = async () => {
+    try {
+      const sellData = {
+        value: totalValue,
+        paymentMethodId: Number(paymentMethodId),
+        dishes: sendDish,
+        drinks: sendDrink,
+      };
+
+      const response = await axios.post(
+        `${baseUrl}/sells`,
+        sellData,
+        authToken
+      );
+      
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadDishs();
+    loadDrinks();
+  }, []);
+
   return (
     <div className={styles.mainOrderPage}>
       <div className={styles.leftOrderPage}>
         <h2>Selecione seus itens:</h2>
         <div className={styles.form}>
-          <Select text={"Selecione um prato"} />
-          <Input placeholder={"Selecione a quantidade"} />
-          <Button text={"Adicionar"} type={"login"} />
+          {drinks && dishs && (
+            <>
+              <Select
+                text={"Selecione um prato"}
+                options={dishs}
+                handleSelect={handleDishMenu}
+                name={"dishId"}
+              />
+              <Input
+                placeholder={"Selecione a quantidade"}
+                name={"quantity"}
+                handleChange={handleDishMenu}
+              />
+              <Button text={"Adicionar"} type={"login"} handleClick={addDish} />
 
-          <Select text={"Selecione uma bebida"} />
-          <Input placeholder={"Selecione a quantidade"} />
-          <Button text={"Adicionar"} type={"login"} />
+              <Select
+                text={"Selecione uma bebida"}
+                options={drinks}
+                handleSelect={handleDrinkMenu}
+                name={"drinkId"}
+              />
+              <Input
+                placeholder={"Selecione a quantidade"}
+                name={"quantity"}
+                handleChange={handleDrinkMenu}
+                type={"number"}
+              />
+              <Button
+                text={"Adicionar"}
+                type={"login"}
+                handleClick={addDrink}
+              />
 
-          <Select text={"Método de pagamento"} options={options}/>
+              <Select
+                text={"Método de pagamento"}
+                options={options}
+                handleSelect={handlePaymentMethod}
+              />
+              <Button
+                text={"Finalizar venda"}
+                type={"login"}
+                handleClick={makeSell}
+              />
+            </>
+          )}
         </div>
         <div className={styles.orderResume}>
           <h2>Resumo do pedido</h2>
@@ -63,7 +198,7 @@ function OrderPage() {
             }}
           />
 
-          <p>Total 20</p>
+          <p>{totalValue}</p>
         </div>
       </div>
     </div>
