c1090e77ea7ac70128eefe089accf4edd83c59b2
FabricioSobrinho <fabriciomails3@gmail.com>
2024-11-16 16:25:59 -0400

    Feat: create the correct menu render itens

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,9 +1,30 @@
-function OrderItem({ item }) {
+function OrderItem({ order }) {
   return (
     <div>
-      <p>Nome: {item.nome}</p>
-      <p>Quantidade: {item.quantidade}</p>
-      <p>Valor: {item.valor}</p>
+      {order.dishs && order.dishs.length > 0 && (
+        <>
+          <h4>Pratos</h4>
+          {order.dishs.map((item, index) => (
+            <div key={index}>
+              <p>Nome: {item.name}</p>
+              <p>Quantidade: {item.quantity}</p>
+              <p>Preço: {item.price}</p>
+            </div>
+          ))}
+        </>
+      )}
+      {order.drinks && order.drinks.length > 0 && (
+        <>
+          <h4>Bebidas</h4>
+          {order.drinks.map((item, index) => (
+            <div key={index}>
+              <p>Nome: {item.name}</p>
+              <p>Quantidade: {item.quantity}</p>
+              <p>Preço: {item.price}</p>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
