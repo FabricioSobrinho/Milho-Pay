fe2f33a73bbdb5dfdd7e96a2fff62291dbc2b120
FabricioSobrinho <fabriciomails3@gmail.com>
2024-12-28 16:47:20 -0400

    Feat: created unlogged navbar

---
+++ src/components/NavbarUnlogged.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/navbar.module.css";
+import Button from "./forms/Button";
+
+function NavbarUnlogged() {
+  return (
+    <div className={styles.mainNavbarUnlogged}>
+      MilhoPay
+    </div>
+  );
+}
+
+export default NavbarUnlogged;
