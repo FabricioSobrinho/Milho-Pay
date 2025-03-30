1fd1efcdc7bd64c32a16f205a6221639b1ece453
FabricioSobrinho <fabriciomails3@gmail.com>
2025-03-30 17:23:11 -0400

    Feat: add value field

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,7 +1,7 @@
 import styles from "../../styles/InputStyles.module.css";
 
 // eslint-disable-next-line react/prop-types
-function Input({ placeholder, handleChange, type, name }) {
+function Input({ placeholder, handleChange, type, name, value }) {
   return (
     <div className={styles.input}>
       <input
@@ -9,6 +9,7 @@ function Input({ placeholder, handleChange, type, name }) {
         onChange={handleChange}
         type={type}
         name={name}
+        value={value}
       />
     </div>
   );
