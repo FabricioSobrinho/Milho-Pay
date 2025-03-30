a718a737d7f5024cd51b7d191f7a2ba52dcde008
FabricioSobrinho <fabriciomails3@gmail.com>
2025-03-30 17:21:05 -0400

    Feat: add success message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,33 @@
-import styles from "../styles/Message.module.css";
+// import hooks
+import { useEffect, useState } from "react";
 
-function Message({ messageType, content }) {
+// import styles
+import styles from "../Styles/Message.module.css";
+
+function Message({ content, messageType }) {
+  const time = 3000;
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (!content) {
+      setVisible(false);
+      return;
+    }
+
+    setVisible(true);
+    if (time) {
+      const timer = setTimeout(() => {
+        setVisible(false);
+      }, time);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [content, time]);
   return (
-    <div className={`${styles.Message} ${styles[messageType]}`}>{content}</div>
+    <>
+      {visible && <div className={`${styles.Message} ${styles[messageType]}`}>{content}</div>}
+    </>
   );
 }
 
