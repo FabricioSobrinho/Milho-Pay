2fab38e9c737f31206ef0ecd0b49de588c43ab7d
FabricioSobrinho <fabriciomails3@gmail.com>
2025-04-18 17:31:11 -0400

    Feat: add navigate to buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,30 @@
 import styles from "../styles/navbar.module.css";
 import Button from "../components/forms/Button";
 import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const tentName = Cookies.get("loggedTent");
-
+  const navigate = useNavigate();
   return (
     <div className={styles.mainNavbar}>
-      <div className={styles.leftNavbar}>{tentName}</div>
+      <div className={styles.leftNavbar} onClick={() => navigate("/tent")}>{tentName}</div>
       <div className={styles.rightNavbar}>
-        <Button text="Pratos" type="nav" />
-        <Button text="Bebidas" type="nav" />
-        <Button text="Vendas" type="nav" />
+        <Button
+          text="Pratos"
+          type="nav"
+          handleClick={() => navigate("/menu")}
+        />
+        <Button
+          text="Bebidas"
+          type="nav"
+          handleClick={() => navigate("/drink")}
+        />
+        <Button
+          text="Vendas"
+          type="nav"
+          handleClick={() => navigate("/order")}
+        />
       </div>
     </div>
   );
